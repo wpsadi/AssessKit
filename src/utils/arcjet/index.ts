668e554e6ff3d861d8d55825e4615545ca7b3a6a
wpsadi <wpsadi@outlook.com>
2025-07-10 14:39:26 +0530

    save

--- src/utils/arcjet/index.ts
+++ src/utils/arcjet/index.ts
@@ -10,19 +10,19 @@ const aj = arcjet({
 		// Shield protects your app from common attacks e.g. SQL injection
 		shield({ mode: "LIVE" }),
 		// Create a bot detection rule
-		detectBot({
-			mode: "LIVE", // Blocks requests. Use "DRY_RUN" to log only
-			// Block all bots except the following
-			allow: [
-				"CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
-				// Uncomment to allow these other common bot categories
-				// See the full list at https://arcjet.com/bot-list
-				//"CATEGORY:MONITOR", // Uptime monitoring services
-				//"CATEGORY:PREVIEW", // Link previews e.g. Slack, Discord,
-				// allow postman
-				"CATEGORY:MONITOR",
-			],
-		}),
+		// detectBot({
+		// 	mode: "LIVE", // Blocks requests. Use "DRY_RUN" to log only
+		// 	// Block all bots except the following
+		// 	allow: [
+		// 		"CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
+		// 		// Uncomment to allow these other common bot categories
+		// 		// See the full list at https://arcjet.com/bot-list
+		// 		"CATEGORY:MONITOR", // Uptime monitoring services
+		// 		"CATEGORY:PREVIEW", // Link previews e.g. Slack, Discord,
+		// 		// allow postman
+		// 		"CATEGORY:MONITOR",
+		// 	],
+		// }),
 		// Create a token bucket rate limit. Other algorithms are supported.
 		// 100 token for one minute
 		tokenBucket({
